4467424942dd6cebb5ea4b4e8ddb169ef3fa512d
matheu46 <mathias.matheus@ymail.com>
2024-06-05 21:11:32 -0300

    removido contato e sobre

--- recipes/views.py
+++ recipes/views.py
@@ -1,12 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse('HOME')
-
-def contato(request):
-    return HttpResponse('Contato')
-
-def sobre(request):
-    return HttpResponse('Sobre')
+    return render(request, 'recipes/home.html')
