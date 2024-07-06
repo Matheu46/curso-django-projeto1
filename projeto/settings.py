771979b5ebcd67599c1315e40b7b4d1d7aa84af3
matheu46 <mathias.matheus@ymail.com>
2024-07-06 14:26:11 -0300

    dados com o faker

--- projeto/settings.py
+++ projeto/settings.py
@@ -56,7 +56,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            
+            BASE_DIR / 'base_template'
         ],
         'APP_DIRS': True,
         'OPTIONS': {
